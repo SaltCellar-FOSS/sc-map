859f6adf4b05d499dc732ab4a541043dee0180dd
Gabriel Britain <quontas@gmail.com>
2026-03-18 13:17:39 -0500

    Update filter icon to actually be fork/knife

--- src/lib/icons/FilterRestaurantIcon.svelte
+++ src/lib/icons/FilterRestaurantIcon.svelte
@@ -4,21 +4,14 @@
 
 <svg
 	xmlns="http://www.w3.org/2000/svg"
-	width={size}
 	height={size}
-	viewBox="0 0 24 24"
-	fill="none"
-	stroke="currentColor"
+	viewBox="0 -960 960 960"
+	width={size}
+	fill="currentColor"
 	stroke-width="2"
 	stroke-linecap="round"
 	stroke-linejoin="round"
+	><path
+		d="M280-80v-366q-51-14-85.5-56T160-600v-280h80v280h40v-280h80v280h40v-280h80v280q0 56-34.5 98T360-446v366h-80Zm400 0v-320H560v-280q0-83 58.5-141.5T760-880v800h-80Z"
+	/></svg
 >
-	<path d="M3 11V19a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-8" />
-	<path d="M5 11V6a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v5" />
-	<line x1="12" y1="3" x2="12" y2="11" />
-	<path d="M8 15h.01" />
-	<path d="M8 19h.01" />
-	<path d="M16 15h.01" />
-	<path d="M16 19h.01" />
-	<path d="M12 19h.01" />
-</svg>
