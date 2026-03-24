cbc23f8a4dadeb2542ff24bf7d6af9aa6648c67a
Gabriel Britain <quontas@gmail.com>
2026-03-24 12:21:39 -0500

    Drop back button

--- src/lib/components/ui/search-view/SearchView.svelte
+++ src/lib/components/ui/search-view/SearchView.svelte
@@ -247,23 +247,6 @@
 	>
 		<!-- Header -->
 		<div class="md-search-view__header">
-			<!-- Back button -->
-			<button
-				class="md-search-view__back"
-				aria-label="Close search"
-				type="button"
-				onclick={closeView}
-			>
-				<svg
-					class="md-search-view__back-icon"
-					viewBox="0 0 24 24"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
-				</svg>
-			</button>
-
 			<!-- Live input -->
 			<input
 				class="md-search-view__input"
