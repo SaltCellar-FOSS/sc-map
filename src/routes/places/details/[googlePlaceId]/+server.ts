4ed0b5c446a915829898f88acd7d416983ce0fc5
Gabriel Britain <quontas@gmail.com>
2026-03-24 00:25:56 -0500

    Add auth guard

--- src/routes/places/details/[googlePlaceId]/+server.ts
+++ src/routes/places/details/[googlePlaceId]/+server.ts
@@ -1,8 +1,13 @@
 import { getPlaceDetails } from '$lib/server/google-places';
-import { jsonResponse } from '$lib/server/response';
+import { verifySessionCookie } from '$lib/server/cookie';
+import { jsonResponse, errorResponse } from '$lib/server/response';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({ params, cookies }) => {
+	const sessionCookie = cookies.get('session');
+	const userId = sessionCookie ? await verifySessionCookie(sessionCookie) : null;
+	if (!userId) return errorResponse('Unauthorized', 401);
+
 	const details = await getPlaceDetails(params.googlePlaceId!);
 	return jsonResponse(details);
 };
