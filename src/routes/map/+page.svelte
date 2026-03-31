f4ad519e2b40d2bbd49693564efeccbe795e8609
Ignacio Tampe <ignacio.tampepalma@telus.com>
2026-03-31 13:39:22 -0700

    fix: configure svelte/no-navigation-without-resolve to allow pushState/replaceState for shallow query-param routing

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -12,7 +12,6 @@
 	import type { SavedPlace } from '$lib/schemas/saved-place';
 	import type { VisitWithUser } from '$lib/schemas/visit';
 	import { invalidate, pushState, replaceState } from '$app/navigation';
-	import { resolve } from '$app/paths';
 	import { page } from '$app/stores';
 	import { slugifyPlace, parsePlaceId } from '$lib/place-slug';
 	import type VisitDialogType from '$lib/components/VisitDialog.svelte';
@@ -51,12 +50,12 @@
 		if (isSavedPlace(place)) {
 			visitsResult = getVisitsForPlace(place.id);
 			sheetOpen = true;
-			const resolvedUrl = resolve(`${$page.url.pathname}?place=${slugifyPlace(place)}`);
+			const url = `${$page.url.pathname}?place=${slugifyPlace(place)}`;
 			selectedPlace = place;
 			if ($page.url.searchParams.has('place')) {
-				replaceState(resolvedUrl, {});
+				replaceState(url, {});
 			} else {
-				pushState(resolvedUrl, {});
+				pushState(url, {});
 			}
 		} else {
 			selectedPlace = place;
